b94d26ae4a9b02912ab280302d018b7ef7845b2e
aleeha0001 <aleehaali0001@gmail.com>
2024-08-16 21:31:11 +0500

    Added Providers List

---
+++ backend/routes/providers.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const providerController= require('../controllers/providers');
+
+/* GET list page. */
+router.get('/', providerController.list);
+// router.get('/about', mainController.about);
+// router.get('/contact', mainController.contact);
+// router.get('/login', mainController.login);
+// router.get('/register', mainController.register);
+// router.get('/forgot-password', mainController.forgotpassword);
+
+module.exports = router;

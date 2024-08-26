efa2e0d5dbec07fcfd166f3bea2d1ebab69c3e9f
aleeha0001 <aleehaali0001@gmail.com>
2024-08-26 20:27:26 +0500

    Updating Documents in the MongoDB Using findOneAndUpdate()

--- backend/api/controllers/main.controller.js
+++ backend/api/controllers/main.controller.js
@@ -84,28 +84,25 @@ module.exports.readOne = function (req, res) {
 // PUT
 // uri: /api/providers/:id
 module.exports.update = function (req, res) {
-    if (isEmptyList(providers)) {
-        res.status(404);
-        res.send('List is empty. Cannot update.');
-    }
-    let id = req.params.id;
-    let provider = providers.find(provider => provider.id == id);
-    provider.firstname = req.body.firstname;
-    provider.lastname = req.body.lastname;
-    provider.position = req.body.position;
-    provider.company.company_name = req.body.company.company_name;
-    provider.company.address = req.body.company.address;
-    provider.company.address2 = req.body.company.address2;
-    provider.company.city = req.body.company.city;
-    provider.company.state = req.body.company.state;
-    provider.company.postal_code = req.body.company.postal_code;
-    provider.company.phone = req.body.company.phone;
-    provider.company.email = req.body.company.email;
-    provider.company.description = req.body.company.description;
-    provider.company.tagline = req.body.company.tagline;
+    try {
+        let id = new ObjectId(req.params.id);
+        let provider = req.body;
+        Provider.findOneAndUpdate({ '_id': id }, provider, { new: true })
+            .then(result => {
+                if (isEmptyList(result)) {
+                    res.status(404);
+                    res.send('List is empty. Cannot update.');
+                }
+                res.status(200);
+                res.send(result);
 
-    res.status(200);
-    res.send(provider);
+            })
+            .catch(error => handleError(res, error));
+
+    }
+    catch (error) {
+        handleError(res, error)
+    };
 
 };
 // DELETE ONE
